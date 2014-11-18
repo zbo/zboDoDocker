157d642896df21e45d835f712b6346f64f0c6779
superzbo <zbo2@qq.com>
2014-11-18 12:51:10 +0800

    init env

---
+++ initTest/src/test/initTest.java
@@ -0,0 +1,14 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by twer on 14-11-18.
+ */
+
+public class initTest {
+    @Test
+    public void firstTest(){
+        System.out.println("start to do docker from here");
+        Assert.assertEquals("a", "a");
+    }
+}
